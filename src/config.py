ae1be9e0d06a246011bb8e774a2f7dcb188a7eaf
colganwi <colgan.william@gmail.com>
2024-04-22 14:29:06 -0400

    added cmap

--- src/config.py
+++ src/config.py
@@ -1,5 +1,11 @@
 from pathlib import Path
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
 
 threads = 30
 log_path = None
 default_colors = ["black","blue","red","green","orange"]
+
+# Configure colormaps
+cmap_colors = [(1, 1, 1)] + [plt.cm.Reds(i / (256 - 1)) for i in range(256)]
+sequential_cmap = mcolors.LinearSegmentedColormap.from_list("Reds", cmap_colors, N=256)
