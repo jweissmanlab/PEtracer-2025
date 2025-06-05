8afc7eee562bcf9690439bc8687e31817cae548a
Katie Yost <30815536+katieyost@users.noreply.github.com>
2025-06-04 21:01:02 -0400

    update processing script

--- strategy_selection/insertvariants/crispresso.sh
+++ strategy_selection/insertvariants/crispresso.sh
@@ -6,7 +6,7 @@ f="$(basename -- $line)"
 dir="$(dirname -- $line)"
 CWD=$PWD
 cd $dir
-sbatch --job-name="$dir1"_crispresso --nodes=1 --ntasks=1 --cpus-per-task=20 --mem=50gb --partition=20 --wrap "CRISPRessoBatch --batch_settings $f"
+sbatch --job-name="${f%.txt}"_crispresso --nodes=1 --ntasks=1 --cpus-per-task=20 --mem=50gb --partition=20 --wrap "CRISPRessoBatch --batch_settings $f --suppress_plots "
 cd $CWD
 done < CRISPResso_batch_files.txt
 
